5f6a6328acc79cdc2ca50b00514e47f6709158b4
Dan Fornika <dfornika@gmail.com>
2019-03-12 22:03:58 -0700

    Move scripts to scripts dir

--- setup.py
+++ setup.py
@@ -9,7 +9,15 @@ setup(
     author_email='',
     license='MIT',
     packages=['uvp'],
-    scripts=['bin/uvp'],
+    scripts=[
+        'bin/uvp',
+        'scripts/coverage_estimator.py',
+        'scripts/del_parse.py',
+        'scripts/lineage_parser.py',
+        'scripts/parse_annotation.py',
+        'scripts/raw_vcf_parse.py',
+        'scripts/resis_parser.py',
+    ],
     zip_safe=False,
     install_requires = [
         "pyyaml",
